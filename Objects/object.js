15a4e69e3dfbdef003093d419236d8c56338120d
maheshkatti19 <maheshk9668@gmail.com>
2019-08-20 18:34:02 +0530

    first commit

--- Objects/object.js
+++ Objects/object.js
@@ -5,23 +5,67 @@
 // console.log(y);
 
 
-var pizza ={
-    crust:'thin',
-    toppings:3,
-    hasBacon:true,
-    howmanyTOppings:function(){
-        return this.toppings;
+// var pizza ={
+//     crust:'thin',
+//     toppings:3,
+//     hasBacon:true,
+//     howmanyTOppings:function(){
+//         return this.toppings;
 
-    }
+//     }
+// };
+// // add the object 
+// pizza.price='10$';
+// // console.log(pizza.howmanyTOppings());
+
+// // deleting object data
+// // delete(pizza.crust);
+
+// // console.log(pizza);
+
+
+// object to object accessing data
+
+
+var pizza =function(){
+   this. crust='thin';
+   this. toppings=3;
+   this. hasBacon=true;
+  
+
+    
 };
-// add the object 
-pizza.price='10$';
-// console.log(pizza.howmanyTOppings());
+var pizzaA =new pizza();
+var pizzaB =new pizza();
+pizzaA.crust="pan";
+// console.log(pizzaA.crust);
 
-// deleting object data
-delete(pizza.crust);
-
-console.log(pizza);
+// console.log(pizzaB instanceof pizza);
+console.log(pizzaB.constructor);
 
 
-// object to object 
+
+var pizza =function(){
+    var crust ='thin';
+    // this.hasBacon=true;
+    var toppings=3;
+// this.getHasBacon=function(){
+//     return this.hasBacon;
+// };
+// this.getcrust= function(){
+//     return crust;
+// };
+
+var getToppings =function(){
+    return toppings;
+};
+var tmp={}
+tmp.getToppings=getToppings;
+return tmp;
+
+};
+var pizzaA = new pizza();
+console.log(pizzaA.getToppings());
+// console.log(pizzaA.getHasBacon());
+// console.log(pizzaA.getcrust());
+
